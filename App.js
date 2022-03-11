6dce286344cf1c4db9a3e668cbadd29bd032f511
SonaliPKoli <86517758+SonaliPKoli@users.noreply.github.com>
2022-03-11 18:50:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {Restaurant, OrderDelivery, Home} from './screens';
+import Tabs from './Navigation/tabs';
+const Stack = createStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          headerTintColor: 'white',
+          headerStyle: {backgroundColor: 'tomato'},
+        }}
+        initialRouteName={'Tabs'}>
+        <Stack.Screen name="Tabs" component={Tabs} />
+        <Stack.Screen name="Restaurant" component={Restaurant} />
+        <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default App;
